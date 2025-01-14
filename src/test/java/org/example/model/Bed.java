94aab62aac41899e6c39e8c311653c21b24ca172
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2025-01-14 11:36:53 +0300

    Bed classı oluşturuldu

---
+++ src/test/java/org/example/model/Bed.java
@@ -0,0 +1,43 @@
+package org.example.model;
+
+public class Bed {
+    private int pillows;
+    private int height;
+    private int sheets;
+    private int quilt;
+    private String style;
+
+
+
+    public Bed(int pillows, int height, int sheets, int quilt, String style) {
+        this.pillows = pillows;
+        this.height = height;
+        this.sheets = sheets;
+        this.quilt = quilt;
+        this.style = style;
+    }
+
+    public int getPillows() {
+        return pillows;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getSheets() {
+        return sheets;
+    }
+
+    public int getQuilts() {
+        return quilt;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void make () {
+      System.out.println("The bed is being made.");
+    }
+}
